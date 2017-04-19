f377649205367135de3a4266069f96ed9ad87557
Shravan kumar reddy vanteru <shravanvanteru@gmail.com>
2017-04-19 20:06:05 +0530

    Added the SpringActuator Sample Project

---
+++ SpringActuator1/src/main/java/com/example/SpringActuator1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuator1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuator1Application.class, args);
+	}
+}
